e83763813086888f79436139b34e1bd95b5dd440
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2023-10-22 20:23:30 +0200

    Add context

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
+import { SearchContext } from "../../contexts/search.context";
 import "src/Map";
 import "../../utils/fix-map-icon";
-
 import "leaflet/dist/leaflet.css";
 import "./Map.css";
 
 export const Map = () => {
+  const { search } = useContext(SearchContext);
+
+  useEffect(() => {
+    console.log("Make request to search for", search);
+  }, [search]);
+
   return (
     <div className="map">
+      <h1>Search for: {search}</h1>
       <MapContainer center={[50.2657152, 18.9945008]} zoom={15}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,20 +1,31 @@
-import React from "react";
-import "./App.css";
+import React, { SyntheticEvent, useContext, useState } from "react";
+import "./Header.css";
 import { Btn } from "../common/Btn";
+import { SearchContext } from "../../contexts/search.context";
 
 export const Header = () => {
+  const { search, setSearch } = useContext(SearchContext);
+  const [inputVal, setInputVal] = useState(search);
+
+  const setSearchFromLocalState = (e: SyntheticEvent) => {
+    e.preventDefault();
+    setSearch(inputVal);
+  };
+
   return (
-    <>
-      <header>
-        <h1>
-          <strong>Ogłoszenia</strong>
-        </h1>
-        <Btn text="Dodaj ogłoszenie"></Btn>
-        <div className="search">
-          <input type="search" />
-          <Btn text="Szukaj" />
-        </div>
-      </header>
-    </>
+    <header>
+      <h1>
+        <strong>Ogłoszenia</strong>
+      </h1>
+      <Btn text="Dodaj ogłoszenie"></Btn>
+      <div className="search" onSubmit={setSearchFromLocalState}>
+        <input
+          type="text"
+          value={inputVal}
+          onChange={(e) => setInputVal(e.target.value)}
+        />
+        <Btn text="Szukaj" />
+      </div>
+    </header>
   );
 };
